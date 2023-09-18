46b20ec8a518462ba6f408fe2b7068e89e0e829a
dimakryshtal <dima.krshl@gmail.com>
2023-09-18 14:47:23 +0300

    allowed all headers

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: [
-      'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Content-Type',
-      'accept',
-    ],
     origin: '*',
     credentials: true,
   });
