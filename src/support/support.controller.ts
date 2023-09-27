c5f6680f3d5f0352f79ca7bb945a00984cb89ce2
dimakryshtal <dima.krshl@gmail.com>
2023-09-27 18:18:01 +0300

    fixed roles for support

--- src/support/support.controller.ts
+++ src/support/support.controller.ts
@@ -28,12 +28,12 @@ import { Support } from './entities/support.entity';
 
 @ApiTags('Support')
 @ApiBearerAuth()
-@Permissions('support')
 @UseGuards(AuthGuard('jwt'), RolesGuard)
 @Controller('support')
 export class SupportController {
   constructor(private readonly supportService: SupportService) {}
 
+  @Permissions('support')
   @ApiOperation({ summary: 'Create a new support record in the system' })
   @ApiOkResponse({ description: 'A support record has been created successfully', type: Support })
   @Post()
@@ -41,8 +41,6 @@ export class SupportController {
     return this.supportService.create(req.user.user_id, createSupportDto);
   }
 
-  @Permissions('superuser')
-  @UseGuards(RolesGuard)
   @ApiOperation({ summary: 'Get all support records in the system' })
   @ApiOkResponse({ description: 'All support records have been received', type: [Support] })
   @Get()
@@ -50,8 +48,6 @@ export class SupportController {
     return this.supportService.findAll();
   }
 
-  @Permissions('superuser')
-  @UseGuards(RolesGuard)
   @ApiOperation({ summary: 'Get a support record by its id' })
   @ApiOkResponse({ description: 'The support record has been received', type: Support })
   @Get(':id')
@@ -59,8 +55,6 @@ export class SupportController {
     return this.supportService.findOne(+id);
   }
 
-  @Permissions('superuser')
-  @UseGuards(RolesGuard)
   @ApiOperation({ summary: 'Update a support record by its id' })
   @ApiOkResponse({ description: 'The support record has been updated successfully', type: Support })
   @Patch(':id')
@@ -68,8 +62,6 @@ export class SupportController {
     return this.supportService.update(+id, updateSupportDto);
   }
 
-  @Permissions('superuser')
-  @UseGuards(RolesGuard)
   @ApiOperation({ summary: 'Delete a support record by its id' })
   @ApiNoContentResponse({ description: 'The support record has been deleted successfully' })
   @HttpCode(HttpStatus.NO_CONTENT)
