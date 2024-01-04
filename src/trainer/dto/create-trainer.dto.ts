ac251c239da0e79e786eae3fcbf1c5cfa8471c0f
Nazik <nvovk.2004@gmail.com>
2024-01-04 17:06:51 +0200

    fix: exclude email field from update manager and trainer dto, fix manager role

--- src/trainer/dto/create-trainer.dto.ts
+++ src/trainer/dto/create-trainer.dto.ts
@@ -1,86 +1,114 @@
-import { Min, Max, ValidateIf, IsMobilePhone } from 'class-validator';
+import {
+  Min,
+  Max,
+  ValidateIf,
+  IsMobilePhone,
+  IsEmail,
+  IsString,
+  IsOptional,
+  IsDateString,
+  IsNumber,
+} from 'class-validator';
 
 export class CreateTrainerDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
   first_name: string;
+
+  @IsString()
   last_name: string;
 
   @IsMobilePhone('uk-UA')
   phone: string;
 
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
   image?: string;
-  hired_at?: Date;
-  resigned_at?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  hired_at?: string;
+
+  @IsDateString()
+  @IsOptional()
+  resigned_at?: string;
+
+  @IsNumber()
   price_per_hour: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   monday_start_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   monday_end_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   tuesday_start_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   tuesday_end_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   wednesday_start_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   wednesday_end_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   thursday_start_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   thursday_end_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   friday_start_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   friday_end_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   saturday_start_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   saturday_end_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   sunday_start_hours?: number;
 
   @Min(0)
   @Max(24)
-  @ValidateIf((_, value) => (value ? true : false))
+  @ValidateIf((_, value) => !!value)
   sunday_end_hours?: number;
 }
--- src/trainer/dto/update-trainer.dto.ts
+++ src/trainer/dto/update-trainer.dto.ts
@@ -1,4 +1,114 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTrainerDto } from './create-trainer.dto';
+import {
+  Min,
+  Max,
+  ValidateIf,
+  IsMobilePhone,
+  IsString,
+  IsOptional,
+  IsDateString,
+  IsNumber,
+} from 'class-validator';
 
-export class UpdateTrainerDto extends PartialType(CreateTrainerDto) {}
+export class UpdateTrainerDto {
+  @IsString()
+  @IsOptional()
+  first_name?: string;
+
+  @IsString()
+  @IsOptional()
+  last_name?: string;
+
+  @IsMobilePhone('uk-UA')
+  @IsOptional()
+  phone?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  image?: string;
+
+  @IsDateString()
+  @IsOptional()
+  hired_at?: string;
+
+  @IsDateString()
+  @IsOptional()
+  resigned_at?: string;
+
+  @IsNumber()
+  @IsOptional()
+  price_per_hour?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  monday_start_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  monday_end_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  tuesday_start_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  tuesday_end_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  wednesday_start_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  wednesday_end_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  thursday_start_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  thursday_end_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  friday_start_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  friday_end_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  saturday_start_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  saturday_end_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  sunday_start_hours?: number;
+
+  @Min(0)
+  @Max(24)
+  @ValidateIf((_, value) => !!value)
+  sunday_end_hours?: number;
+}
